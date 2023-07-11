7a13f413bef9f18bd8d5984d7bf982e99c893674
toktokz1 <jiramet99@gmail.com>
2023-07-11 21:51:54 +0700

    kuy01
    jjjjj

--- src/components/Appheader.js
+++ src/components/Appheader.js
@@ -3,7 +3,7 @@ import './Appheader.css';
 function AppHeader() {
     return (
         <header className="app-header">
-            <h4>Just 22233Row</h4>
+            <h4>Just 888833Row</h4>
         </header>
     );
 }
